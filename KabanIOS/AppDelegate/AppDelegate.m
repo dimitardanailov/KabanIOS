ccbe501d9006218e7e159187de109d6ce6247dda
Dimitar Danailov <dimityr.danailov@gmail.com>
2018-08-22 18:00:26 +0300

    Application has a facebook login button.

--- KabanIOS/AppDelegate/AppDelegate.m
+++ KabanIOS/AppDelegate/AppDelegate.m
@@ -6,6 +6,8 @@
 //  Copyright © 2018 Dimitar Danailov. All rights reserved.
 //
 
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
@@ -16,10 +18,29 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    [[FBSDKApplicationDelegate sharedInstance] application:application
+                             didFinishLaunchingWithOptions:launchOptions];
+    
     return YES;
 }
 
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+            options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+    
+    BOOL handled = [[FBSDKApplicationDelegate sharedInstance]
+        application:application
+        openURL:url
+        sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]
+        annotation:options[UIApplicationOpenURLOptionsAnnotationKey]
+    ];
+    // Add any custom logic here.
+    return handled;
+}
+    
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    
+}
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -36,12 +57,6 @@
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
 }
-
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
--- KabanIOS/ViewControllers/WelcomeView/WelcomeViewController.m
+++ KabanIOS/ViewControllers/WelcomeView/WelcomeViewController.m
@@ -6,6 +6,9 @@
 //  Copyright © 2018 Dimitar Danailov. All rights reserved.
 //
 
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
 #import "WelcomeViewController.h"
 
 @interface WelcomeViewController ()
@@ -16,13 +19,27 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self loadFacebookSignButton];
+    
+    if ([FBSDKAccessToken currentAccessToken]) {
+        NSLog(@"User is logged through Facebook ...");
+    }
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+    
+- (void)loadFacebookSignButton {
+    FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
+    // Optional: Place the button in the center of your view.
+    loginButton.center = self.view.center;
+    loginButton.readPermissions = @[@"public_profile", @"email"];
+    
+    [self.view addSubview:loginButton];
 }
 
 
